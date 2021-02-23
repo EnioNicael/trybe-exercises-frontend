78242dc115db13b4781c84f875e5378137e763dd
Enio Nicael <enionicael@gmail.com>
2021-02-23 19:22:44 -0300

    Correcoes do code climate

--- estados-e-eventos_1/exercise-pokedex-state/src/App.js
+++ estados-e-eventos_1/exercise-pokedex-state/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import Button from './Button';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-        index: 0
+        index: 0,
+        filteredPokemons: []
     }
   }
 
   handleClick = () => {
     this.setState((previousState, _props) => {
+      //if (previousState.index === filteredPokemons.length - 1) {
       if (previousState.index === pokemons.length - 1) {
         return { index: 0 };
       }
@@ -20,12 +23,25 @@ class App extends React.Component {
     });
   }
 
+  filtersArrayOfPokemon = () => {
+    this.setState((previousState, _props) => {
+      previousState = pokemons.filter(pokemon => pokemon.type === 'Psychic');
+      console.log('previousState');
+      return { filteredPokemons: previousState }
+    })
+  }
+
   render() {
-    console.log(this.state.index);
+    //this.filteredPokemons = ;
+    this.filtersArrayOfPokemon();
+    console.log(this.state);
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons[this.state.index]} />
+        <Pokedex pokemons={this.filteredPokemons[this.state.index]} />
+        <div>
+          <Button value={'Psychic'} />
+        </div>
         <button onClick={this.handleClick}>Next</button>
       </div>
     );
